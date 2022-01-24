9debdf13d86478198ca1274dae5020e2c0bc119d
Jack Setrak <jackfire78@yahoo.com>
2022-01-24 11:05:13 -0700

    Initial-front end code handling

---
+++ src/models/user.js
@@ -0,0 +1,10 @@
+export default class User{
+    constructor (firstName, lastName, username, password, email, phoneNum){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phoneNum = phoneNum;
+    }
+}
