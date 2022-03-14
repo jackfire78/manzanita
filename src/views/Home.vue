3666730c1358a43af120524207d381d0bf8af702
jackfire78 <jackfire78@yahoo.com>
2022-03-14 14:23:06 -0700

    Updated home page titles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,30 +2,25 @@
 
   <div class="ParalaxImage1">
     <img class="card-img-top" src="https://storage.googleapis.com/sagewoodlcs-prod-assets/uploads/7a595f66-1fcba867-sagewood-building.jpg" alt="Card image cap">
-    <div class="ptext">
-      <span class="border ">
-        Parallax Website
-      </span>
-    </div>
   </div>
-
+  <br><br>
   <section class="section section-light">
     <div class="container">
       <div class="row">
         <div class="col-md-4">
-          <h2>Section One</h2>
+          <h2>Who are we?</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi,
             fugit amet tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium,
             blanditiis sapiente labore aliquam ipsa architecto vitae. Minima soluta temporibus voluptates inventore commodi</p>
         </div>
         <div class="col-md-4">
-          <h2>Section two</h2>
+          <h2>Goals</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi,
             fugit amet tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium,
             blanditiis sapiente labore aliquam ipsa architecto vitae. Minima soluta temporibus voluptates inventore commodi</p>
         </div>
         <div class="col-md-4">
-          <h2>Section three</h2>
+          <h2>Goals</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi,
             fugit amet tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium,
             blanditiis sapiente labore aliquam ipsa architecto vitae. Minima soluta temporibus voluptates inventore commodi</p>
@@ -33,7 +28,7 @@
       </div>
       <div class="row mt-5">
         <div id="section1" class="col-md-6 m-auto">
-          <h2>Section One</h2>
+          <h2>History</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugit amet
             tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium,
@@ -54,7 +49,7 @@
 
         </div>
         <div id="section1" class="col-md-6 m-auto">
-          <h2>Section two</h2>
+          <h2>Independent Living</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugit amet
             tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium,
@@ -69,84 +64,71 @@
             cumque esse suscipit optio aliquam et, dolorem a cupiditate nihil fuga laboriosam fugiat placeat dignissimos!
             Unde eveniet placeat quisquam blanditiis voluptatem doloremque fugiat dolor repellendus ratione in. Distinctio
             provident dolorem modi cumque illo enim quidem tempora deserunt nostrum voluptate labore repellat quisquam quasi
-
-
           </p>
-
         </div>
 
-
       </div>
-
     </div>
-
   </section>
 
+  <br><br>
   <div class="ParalaxImage2">
     <div class="container">
       <div class="row">
         <div class="col-sm-10 m-auto">
-          <h3 id="textwhite">Need a developer to promote your business and to develope an online presence ? Hire me !</h3>
-          <button type="button" class="btn btn-primary btn-lg btn-block mt-5">Hire Me !</button>
-          <br><br>
+          <h3 id="textwhite">Looking for work? Look no further and apply at our carrer sections!</h3>
+          <button type="button" class="btn btn-primary btn-lg btn-block mt-5" disabled>Hire Me!</button>
+          <br><br><br><br>
         </div>
       </div>
-
     </div>
   </div>
 
   <section class=" section section-light ">
-
-
-
     <div class="card-deck">
+
       <div class="card">
         <img class="card-img-top" src="https://www.weitz.com/wp-content/uploads/2017/03/Sagewood-South_Courtyard-1-2000x933.jpg" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Card one</h5>
+          <h5 class="card-title">Pools</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a
             little bit longer.</p>
           <p class="card-text">
-            <small class="text-muted">Last updated 3 mins ago</small>
+            <small class="text-muted">Last updated ___ ago</small>
           </p>
         </div>
       </div>
       <div class="card">
         <img class="card-img-top" src="https://toddassoc.com/wp-content/uploads/2018/05/Sagewood-3.jpg" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Card Two</h5>
+          <h5 class="card-title">Relaxing Accomidations</h5>
           <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
           <p class="card-text">
-            <small class="text-muted">Last updated 3 mins ago</small>
+            <small class="text-muted">Last updated ___ ago</small>
           </p>
         </div>
       </div>
       <div class="card">
         <img class="card-img-top" src="https://toddassoc.com/wp-content/uploads/2022/01/Independent-Living-Putting-Green-1030x579.jpg" alt="Card image cap">
         <div class="card-body">
-          <h5 class="card-title">Card Three</h5>
+          <h5 class="card-title">Putting Course</h5>
           <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even
             longer content than the first to show that equal height action.</p>
           <p class="card-text">
-            <small class="text-muted">Last updated 3 mins ago</small>
+            <small class="text-muted">Last updated ___ ago</small>
           </p>
         </div>
       </div>
+      
     </div>
   </section>
-  <div class="ParalaxImage3 ">
-    <div class="ptext ">
-      <span class="border trans  ">
-        parallax website
-      </span>
-    </div>
-  </div>
 
+  <br><br>
   <section class="section section-light ">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <h2>Section Three</h2>
+          <h2>Assisted Living Accomidations</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugit amet
             tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium,
@@ -159,18 +141,12 @@
             cumque esse suscipit optio aliquam et, dolorem a cupiditate nihil fuga laboriosam fugiat placeat dignissimos!
             Unde eveniet placeat quisquam blanditiis voluptatem doloremque fugiat dolor repellendus ratione in.
           </p>
-          <button type="button" name="" id="" class="btn btn-success" btn-lg="" btn-block="">Call To Action Button</button>   
+          <button type="button" name="" id="" class="btn btn-success" btn-lg="" btn-block="" disabled>Learn more here.</button>   
         </div>
       </div>
     </div>    
   </section>
-  <div class="ParalaxImage1 ">
-    <div class="ptext ">
-      <span class="border ">
-        Parallax website
-      </span>
-    </div>
-  </div>
+
 </template>
 
 <script>
